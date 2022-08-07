f63b3f76ddb8b44243475b1e4a3fc0d73d642fcd
김재욱 <kjwook7522@gmail.com>
2022-08-07 17:31:41 +0900

    [master] index 설정 및 vite 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -8,5 +9,11 @@ export default defineConfig({
   server: {
     host: "localhost",
     port: 9090,
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
